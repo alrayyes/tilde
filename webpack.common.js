e8bbac692aa8cd144a11ad1a000176d7e5122e56
Ryan <ryan@andthensome.nl>
2017-09-28 19:13:18 +0200

    added dev & prod builds

--- webpack.common.js
+++ webpack.common.js
@@ -1,32 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    entry: {
-        app: './src/index.js'
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-        hot: true
-    },
     output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
         publicPath: '/'
     },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            template: './src/index.template.ejs',
-            inject: 'body'
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new UglifyJSPlugin()
-    ],
     module: {
         rules: [
             {
@@ -46,5 +25,19 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: './src/index.template.ejs',
+            inject: 'body'
+        }),
+    ]
 };
